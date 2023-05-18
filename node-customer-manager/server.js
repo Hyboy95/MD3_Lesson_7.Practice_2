31c922a56dc179599115823260332c815895d66e
Hyboy95 <temilator@gmail.com>
2023-05-18 11:46:08 +0700

    Practice_2

--- node-customer-manager/server.js
+++ node-customer-manager/server.js
@@ -1,33 +1,31 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const qs = require('qs');
 
 const server = http.createServer((req,res) => {
-    let dataFile = '';
-    let html = '';
-    fs.readFile('./data/data.txt', 'utf-8', (err, data) => {
-        if(err) {
-            console.log(err.message);
-        }
-        dataFile = data.split(',');
-        dataFile.forEach((value, index) => {
-            html += '<tr>';
-            html += `<td>${index + 1}</td>`;
-            html += `<td>${value}</td>`;
-            html += `<td><button class="btn btn-danger">Delete</button></td>`;
-            html += '</tr>';
-        });
-    })
-
-    fs.readFile('./templates/index.html', 'utf-8', (err, data) => {
-        if(err) {
-            console.log(err.message);
-        }
-        res.writeHead(200, {'Content-Type':'text/html'});
-        data = data.replace('{list-user}', html);
-        res.write(data);
-        res.end();
-    })
+    if (req.method === "GET") {
+        fs.readFile('./templates/index.html', 'utf-8', (err, data) => {
+            if (err) console.log(err.message);
+            res.writeHead(200, {'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        })
+    } else {
+        let data = '';
+        req.on('data', chunk => {
+            data += chunk;
+        })
+        req.on('end', () => {
+            let name = qs.parse(data).name
+            fs.writeFile('./data/data.txt', name, err => {
+                if (err) {
+                    console.log(err.message);
+                }
+                res.end('Create success');
+            })
+        })
+    }
 })
 
 server.listen(3000, 'localhost',() => console.log('Server is running at http://localhost:3000'))
